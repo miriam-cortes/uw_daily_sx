a64396c47f87947f2bd1fd15ef7c535e8d2ab191
Miriam Cortes <miriamc@zillowgroup.com>
2020-06-08 17:37:47 -0700

    using a list of bots

--- send_messages.py
+++ send_messages.py
@@ -5,23 +5,25 @@ from urllib.request import Request, urlopen
 GOOGLE_FORM_LINK = 'https://forms.gle/s3Sn7PHuT8NNtPv86'
 DEFAULT_MESSAGE = f"Please fill out your symptom attestation if you plan on working out today {GOOGLE_FORM_LINK}"
 GROUPME_BOT_IDS = [
-    os.environ.get('GROUPME_TEST_BOT_ID'),
+    'GROUPME_TEST_BOT_ID',
 ]
 
 
 def send_message(msg=DEFAULT_MESSAGE):
-    url = 'https://api.groupme.com/v3/bots/post'
-    data = {
-        'bot_id': GROUPME_BOT_IDS,
-        'text': msg,
-    }
-    try:
-        print(f"sending to {url} with data {data}")
-        request = Request(url, urlencode(data).encode())
-        json = urlopen(request).read().decode()
-    except Exception as e:
-        print(e)
-    print("success")
+    for id in GROUPME_BOT_IDS:
+        url = 'https://api.groupme.com/v3/bots/post'
+        data = {
+            'bot_id': os.environ.get(id),
+            'text': msg,
+        }
+        try:
+            print(f"sending to bot {id}")
+            request = Request(url, urlencode(data).encode())
+            json = urlopen(request).read().decode()
+        except Exception as e:
+            print(e)
+            continue
+        print(f"success sending to bot {id}")
 
 
 send_message()
