45493da27f5002478fb38794f90e23a5c40da171
Miriam Cortes <miriamc@zillowgroup.com>
2020-06-12 12:20:06 -0700

    corrected bot param

--- send_messages.py
+++ send_messages.py
@@ -7,13 +7,14 @@ from constants import DEFAULT_MESSAGE
 def send_message(groupme_bot, form_link, msg=DEFAULT_MESSAGE):
     url = 'https://api.groupme.com/v3/bots/post'
     data = {
-        'bot_id': os.environ.get(groupme_bot),
-        'text': f"{msg} {form_link}",
+        'bot_id': groupme_bot,
+        'text': "{} {}".format(msg, form_link),
     }
     try:
-        print(f"sending to bot {groupme_bot}")
+        print("sending to bot {}".format(groupme_bot))
         request = Request(url, urlencode(data).encode())
         json = urlopen(request).read().decode()
+        print(json)
     except Exception as e:
         print(e)
-    print(f"success sending to bot {groupme_bot}")
+    print("success sending to bot {}".format(groupme_bot))
