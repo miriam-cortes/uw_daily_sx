664ab21a4824a7d71f6a3bb66a5785bed2854c3a
Miriam <mcortes.umiami@gmail.com>
2020-06-12 18:16:36 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ QUARANTINE_MESSAGE = "You have to fill this out every day. " \
                      "If you miss a day you must contact your athletic trainer for the reason why. " \
                      "Missed days DO NOT count toward quarantine unless approved by your " \
                      "athletic trainer or team physician."
-OTHER_MESSAGE = "Fill this out every day if you plan on training on campus. " \
+OTHER_MESSAGE = "Fill this out every day you plan on training on campus. " \
                 "If you do not fill this out before you arrive, YOU WILL BE SENT HOME."
 DEFAULT_MESSAGE = "Please fill out your symptom attestation if you plan on working out today "
