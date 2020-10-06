d56154f0c9e75fa8526fa663bafb2f6869a20cfa
Miriam <mcortes.umiami@gmail.com>
2020-10-05 19:51:07 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,3 +5,4 @@ QUARANTINE_MESSAGE = "You have to fill this out every day. " \
 OTHER_MESSAGE = "Fill this out every day you plan on being in the athletic village for any reason. " \
                 "If you do not fill this out before you arrive, YOU WILL BE SENT HOME."
 DEFAULT_MESSAGE = "Please fill out your symptom attestation if you plan on working on-site today "
+VITABOT_MESSAGE = "This is a reminder to take your vitamins today!"
