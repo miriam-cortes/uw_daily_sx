3a580597ec5baa33f3ad92f0ab1e38a19becf8de
Miriam <mcortes.umiami@gmail.com>
2020-10-05 20:02:47 -0700

    Create VitaBot_Message.py

---
+++ VitaBot_Message.py
@@ -0,0 +1,10 @@
+import os
+from datetime import date
+from send_messages import send_message
+from constants import VITABOT_MESSAGE
+
+# DAILY REMINDER FOR GYMNASTICS TO TAKE THEIR VITAMINS
+send_message(
+    'GYM_VITABOT', # vitabot from heroku configs
+    VITABOT_MESSAGE # message
+)
