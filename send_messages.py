0afae88000ff3026143929771b9ee5160566a27a
Miriam Cortes <miriamc@zillowgroup.com>
2021-03-09 13:29:38 -0800

    shortened logging

--- send_messages.py
+++ send_messages.py
@@ -9,16 +9,15 @@ def send_message(groupme_bot, form_link, msg):
         'bot_id': os.environ.get(groupme_bot),
         'text': "{} {}".format(msg, form_link),
     }
+
+    print("sending to bot {}".format(groupme_bot))
     tries = 4
     for i in range(tries):
         try:
-            print("sending to bot {}".format(groupme_bot))
             request = Request(url, urlencode(data).encode())
-            json = urlopen(request).read().decode()
-            print("success sending to bot {}".format(groupme_bot))
-            print("response: {}".format(json))
+            # json = urlopen(request).read().decode()
+            print("success sending to bot {} on the {} try".format(groupme_bot, i))
             break
         except Exception as e:
-            print("failed sending to bot {}".format(groupme_bot))
-            print(e)
+            print("failed for bot {} on try number {} - error: {}".format(groupme_bot, i, e))
 
