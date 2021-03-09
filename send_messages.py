d9369ccba2cb49a665d9b2ac188b5d701fb022fa
Miriam <mcortes.umiami@gmail.com>
2021-03-08 21:26:50 -0800

    increasing the retries to 4

--- send_messages.py
+++ send_messages.py
@@ -9,7 +9,7 @@ def send_message(groupme_bot, form_link, msg):
         'bot_id': os.environ.get(groupme_bot),
         'text': "{} {}".format(msg, form_link),
     }
-    tries = 3
+    tries = 4
     for i in range(tries):
         try:
             print("sending to bot {}".format(groupme_bot))
